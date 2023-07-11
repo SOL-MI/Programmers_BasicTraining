8cae1c840bc305d27d7a0004d3e0724379471265
Solmi Koh <113079102+SOL-MI@users.noreply.github.com>
2023-07-12 01:36:52 +0900

    Update and rename 09. 홀짝 구분하기 to 09_홀짝 구분하기.js

--- 09_홀짝 구분하기.js
+++ 09_홀짝 구분하기.js
@@ -1,9 +1,8 @@
-문제 설명
+/*문제 설명
 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
 
 제한사항
-1 ≤ n ≤ 1,000
----------------------
+1 ≤ n ≤ 1,000*/
 
 const readline = require('readline');
 const rl = readline.createInterface({
