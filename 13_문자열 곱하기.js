83df3fdeb40929e442e93d94d282f4b2de8bd4e9
Solmi Koh <113079102+SOL-MI@users.noreply.github.com>
2023-07-12 01:38:40 +0900

    Update and rename 13. 문자열 곱하기 to 13_문자열 곱하기.js

--- 13_문자열 곱하기.js
+++ 13_문자열 곱하기.js
@@ -1,4 +1,4 @@
-문제 설명
+/*문제 설명
 문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
 
 제한사항
@@ -12,9 +12,7 @@ my_string은 영소문자로만 이루어져 있습니다.
 예제 1번의 my_string은 "string"이고 이를 3번 반복한 문자열은 "stringstringstring"이므로 이를 return 합니다.
 입출력 예 #2
 
-예제 2번의 my_string은 "love"이고 이를 10번 반복한 문자열은 "lovelovelovelovelovelovelovelovelovelove"이므로 이를 return 합니다.
-----------------------------------
-내 풀이
+예제 2번의 my_string은 "love"이고 이를 10번 반복한 문자열은 "lovelovelovelovelovelovelovelovelovelove"이므로 이를 return 합니다.*/
 
 function solution(my_string, k) {
     var answer = '';
