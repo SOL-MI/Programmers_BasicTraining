abc009f239a1508d5b482aee4bc3fb611d2ed5c7
Solmi Koh <113079102+SOL-MI@users.noreply.github.com>
2023-07-12 01:35:34 +0900

    Update and rename 06. 덧셈식 출력하기 to 06_덧셈식 출력하기.js

--- 06_덧셈식 출력하기.js
+++ 06_덧셈식 출력하기.js
@@ -1,9 +1,7 @@
-문제 설명
+/*문제 설명
 두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
 
-a + b = c
-
--------
+a + b = c*/
 
 const readline = require('readline');
 const rl = readline.createInterface({
